a9e5f98a42e71ccbcb0fec0ea3bee890aafb9f51
mtwyford <mtwyford@users.noreply.github.com>
2017-04-03 10:40:01 -0400

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -11,3 +11,16 @@ public class AddressBook {
 		friends = new Contact[10];
 		numfriends = 0;
     }
+	
+	//add a contact passed in as a parameter
+    public void addContact(Contact c) {
+	    friends[numfriends] = c;
+	    numfriends++;
+    }
+	
+	//print out infor on all contacts using method in Contact class
+    public void printContacts(){
+	    for (int i=0;i<numfriends; i++)
+		    friends[i].printContact();
+    }
+	
